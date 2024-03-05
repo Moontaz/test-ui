935e35120056b091ec80f151bb3ae763374b56e9
Moontaz <154495188+Moontaz@users.noreply.github.com>
2024-03-06 02:16:56 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     // rest of the code
